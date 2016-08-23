fb5e8e5bc18945d9e9d40d1e368d03352ec7be7b
Pranesh <pran1994@gmail.com>
2016-08-22 22:33:49 -0400

    smoothened out rotate code

--- Introductory Practice/Rotate.py
+++ Introductory Practice/Rotate.py
@@ -1,3 +1,4 @@
+#Using numpy for easy array management
 import numpy as np;
 
 t = int(input());
@@ -5,92 +6,87 @@ t = int(input());
 for i in range(1, t+1):
     criteria = input().split(" ");
 
-    manipArr = np.zeros((int(criteria[0]), int(criteria[0])), dtype=np.int);
+    # Load input into array
+    manipArr = np.zeros((int(criteria[0]), int(criteria[0])), dtype=np.str);
     for x in range(0, int(criteria[0])):
         temp = input();
         for y in range(0, int(criteria[0])):
-            if temp[y] is '.':
-                manipArr[x][y] = 0;
-            elif temp[y] is 'R':
-                manipArr[x][y] = 1;
-            elif temp[y] is 'B':
-                manipArr[x][y] = 2;
+            manipArr[x][y] = temp[y];
 
+    # Apply "rotation"
     for x in range(0, int(criteria[0])):
         for y in range(int(criteria[0])-1, -1, -1):
-            if manipArr[x][y] == 0:
+            if manipArr[x][y] == '.':
                 currY = y;
-                while (manipArr[x][currY]!=1 and manipArr[x][currY]!=2 and currY>0):
+                while (manipArr[x][currY]!='R' and manipArr[x][currY]!='B' and currY>0):
                     currY-=1;
 
                 manipArr[x][y] = manipArr[x][currY];
-                manipArr[x][currY] = 0;
+                manipArr[x][currY] = '.';
+
+    # Boolean flags
     redWin = False;
-    blackWin = False;
+    blueWin = False;
+
+    # Integer storing win length
     inarow = int(criteria[1]);
+
+    # Win checker
     for x in range(0, int(criteria[0])):
         for y in range(0, int(criteria[0])):
-            if manipArr[x][y]!=0:
+            if manipArr[x][y]!='.' and (not redWin or not blueWin):
                 if x-inarow >= -1:
-                    countR = 0;
-                    countB = 0;
+                    countTokens = 0;
                     for a in range(x, (x-inarow+1), -1):
-                        if manipArr[a][y] == 1 and manipArr[a-1][y] == 1:
-                            countR+=1;
-                        elif manipArr[a][y] == 2 and manipArr[a-1][y] == 2:
-                            countB+=1;
+                        if manipArr[a][y] == manipArr[a-1][y]:
+                            countTokens+=1;
                         else:
                             break;
-                    if countR == (inarow-1):
-                        redWin = True;
-                    if countB == (inarow-1):
-                        blackWin = True;
+                    if countTokens == (inarow-1):
+                        if manipArr[x][y] == 'R':
+                            redWin = True;
+                        else:
+                            blueWin = True;
                 if y-inarow >= -1:
-                    countR = 0;
-                    countB = 0;
+                    countTokens = 0;
                     for a in range(y, (y-inarow+1), -1):
-                        if manipArr[x][a] == 1 and manipArr[x][a-1] == 1:
-                            countR+=1;
-                        elif manipArr[x][a] == 2 and manipArr[x][a-1] == 2:
-                            countB+=1;
+                        if manipArr[x][a] == manipArr[x][a-1]:
+                            countTokens+=1;
                         else:
                             break;
-                    if countR == (inarow-1):
-                        redWin = True;
-                    if countB == (inarow-1):
-                        blackWin = True;
+                    if countTokens == (inarow-1):
+                        if manipArr[x][y] == 'R':
+                            redWin = True;
+                        else:
+                            blueWin = True;
                 if y-inarow >= -1 and x-inarow >=-1:
-                    countR = 0;
-                    countB = 0;
+                    countTokens = 0;
                     for a,b in zip(range(x, (x-inarow+1), -1), (range(y, (y-inarow+1), -1))):
-                        if manipArr[a][b] == 1 and manipArr[a-1][b-1] == 1:
-                            countR+=1;
-                        elif manipArr[a][b] == 2 and manipArr[a-1][b-1] == 2:
-                            countB+=1;
+                        if manipArr[a][b] == manipArr[a-1][b-1]:
+                            countTokens+=1;
                         else:
                             break;
-                        if countR == (inarow-1):
-                            redWin = True;
-                        if countB == (inarow-1):
-                            blackWin = True;
+                        if countTokens == (inarow-1):
+                            if manipArr[x][y] == 'R':
+                                redWin = True;
+                            else:
+                                blueWin = True;
                 if y-inarow>= -1 and x+inarow <= int(criteria[0]):
-                    countR = 0;
-                    countB = 0;
+                    countTokens = 0;
                     for a,b in zip(range(x, (x+inarow+1)), (range(y, (y-inarow+1), -1))):
-                        if manipArr[a][b] == 1 and manipArr[a+1][b-1] == 1:
-                            countR+=1;
-                        elif manipArr[a][b] == 2 and manipArr[a+1][b-1] == 2:
-                            countB+=1;
+                        if manipArr[a][b] == manipArr[a+1][b-1]:
+                            countTokens+=1;
                         else:
                             break;
-                        if countR == (inarow-1):
-                            redWin = True;
-                        if countB == (inarow-1):
-                            blackWin = True;
+                        if countTokens == (inarow-1):
+                            if manipArr[x][y] == 'R':
+                                redWin = True;
+                            else:
+                                blueWin = True;
 
-    if(blackWin and redWin):
+    if(blueWin and redWin):
         print("Case #{}: Both".format(i));
-    elif (blackWin):
+    elif (blueWin):
         print("Case #{}: Blue".format(i));
     elif (redWin):
         print("Case #{}: Red".format(i));
